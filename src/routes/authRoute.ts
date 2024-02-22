f87c82584c6b2ca42853098e744ae6bc6855a9bd
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-22 21:41:50 +0100

    Add routes for user avatar, token refresh, OTP verification, OTP resend, password reset request, and password reset

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -13,19 +13,25 @@ import {
 
 const router = express.Router();
 
+// Route for user avatar
 router.post("/avatar", returnAvatarId);
 
+// Route for token refresh
 router.post("/refresh", refresh);
 
 // Route for user registration
 router.post("/register", registerUser);
 
+// Route for OTP verification
 router.post("/verify", verifyOTP);
 
+// Route for OTP resend
 router.post("/resend", resendOtp);
 
+// Route for password reset request
 router.post("/request-reset", requestReset);
 
+// Route for password reset
 router.post("/reset-password", passwordReset);
 
 // Route for user login
